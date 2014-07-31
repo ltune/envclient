c376fcdaa8fb6b341b82419aa58d4b2026bc2347
Adam Piotr Furmanczuk <adam@debian-lt>
2014-07-31 04:22:45 +0200

    remote synchronization done, more envclient logic

--- main/__init__.py
+++ main/__init__.py
@@ -4,5 +4,5 @@ if __name__ == '__main__':
     # TODO: provide config file and ConfigurationParser instead of hardcoding
     client = Client(adr='/dev/ttyUSB0', ssh_host='debian-lt', ssh_user='test2', ssh_password=None, 
            ssh_keypath='/home/test1/.ssh/id_rsa', input_path='/tmp/commands', 
-           output_path='/tmp/out22', read_timeout=5, remote_timeout=20)
+           output_path='/tmp/output', remote_busy_path='/tmp/vps1', read_timeout=5, remote_timeout=20)
     client.start()
--- main/client.py
+++ main/client.py
@@ -12,47 +12,61 @@ import sys
 import state
 
 
+CLIENT_PREFIX_COMMAND = 'envclient'
+CLIENT_DELIMITER = '~'
+CLIENT_STOP = "stop"
+CLIENT_LOOP = 'loop'
 
 class Client(Thread):
     
-    def __init__(self, adr, ssh_host, ssh_user, ssh_password, ssh_keypath, input_path, output_path, read_timeout=10, remote_timeout=20):
+    def __init__(self, adr, ssh_host, ssh_user, ssh_password, ssh_keypath, input_path, output_path, 
+                 remote_busy_path, read_timeout=10, remote_timeout=20, loop_timeout=60):
         Thread.__init__(self)
         self.input_path = input_path
         self.output_path = output_path
+        self.remote_busy_path = remote_busy_path
         self._ssh_host = ssh_host
         self._ssh_user = ssh_user
         self._ssh_password = ssh_password
         self._ssh_keypath = ssh_keypath 
         self._serial =  serial.Serial()
         self._ssh_user = ssh_user
-        self.read_timeout = read_timeout
-        self.remote_timeout = remote_timeout
+        self.read_timeout = read_timeout # time for reader to capture answers
+        self.remote_timeout = remote_timeout # timeout to retry remote sync
+        self.loop_timeout = loop_timeout # timeout to restart overall process
+        self._must_wait = True # used for syncronization
+        self._enabled = True # to keep app running
+        # setup serial
         self._serial.setTimeout(0)
         self._serial.baudrate = 115200
         self._serial.bytes = 8
         self._serial.parity = serial.serialutil.PARITY_NONE
         self._serial.stopbits = serial.serialutil.STOPBITS_ONE
         self._serial.port = adr
-        self._serial.open()
-        self._serial.setRTS(level = False)
 
     def run(self):
         """client main method."""
-        self._establish_remote() # open remote connection
-        self._get_remote() # get commands from file at remote location
-        # start serial reader and writer 
-        self._reader = Reader(serial = self._serial, read_timeout=self.read_timeout)
-        self._reader.start()
-        self._writer = Writer(serial = self._serial, commands=self.input_commands)
-        self._writer.start()
-        # wait for writer and reader to finish
-        self._writer.join()
-        self._reader.join()
-        # close serial port
-        self._serial.close()
-        # write file contents to remote location
-        self._deliver_remote() # transfer results to remote
-        self._free_remote() # free access on critical resources
+        while self._enabled:
+            self._establish_remote() # open remote connection
+            self._get_remote() # get commands from file at remote location
+            # listen on serial port:
+            self._serial.open()
+            self._serial.setRTS(level = False)
+            # start serial reader and writer 
+            self._reader = Reader(serial = self._serial, read_timeout=self.read_timeout)
+            self._reader.start()
+            self._writer = Writer(serial = self._serial, commands=self.input_commands)
+            self._writer.start()
+            # wait for writer and reader to finish
+            self._writer.join()
+            self._reader.join()
+            # close serial port
+            self._serial.close()
+            # write file contents to remote location
+            self._deliver_remote() # transfer results to remote
+            self._remove_input_commands() # clear processed commands
+            self._free_remote() # free access on critical resources
+            time.sleep(self.loop_timeout)
     
     def _setup_remote(self):
         self._ssh = paramiko.SSHClient()
@@ -74,28 +88,52 @@ class Client(Thread):
     
     def _synced(self):
         """Get exlusive access on remote command file"""
-        # implement Kessels` algorithm with remote ssh connection
-        return True
-    
+        _stdin, stdout, _stderr = self._ssh.exec_command("cat {}".format(self.remote_busy_path))
+        busy = ''.join(stdout.readlines())
+        if '1' in busy:
+            return False
+        else:
+            return True
+
     def _free_remote(self):
         """Free exclusive access on remote files"""
-        # TODO: implement Kessels Alogrithm exit
-        pass
-    
+        self._ssh.close()
+
     def _close_remote(self):
-        self._ssh.close()    
-    
+        self._ssh.close()
+
     def _get_remote(self):
         _stdin, stdout, _stderr = self._ssh.exec_command("cat {}".format(self.input_path))
         commands = stdout.readlines()
+        env_commands = []
         self.input_commands =  [line.replace('|r','\n').replace('\n','') for line in commands if len(line)>0]
-        print self.input_commands
-        
+        # remove commands that are for envclient
+        j = 0
+        while j < len(self.input_commands):
+            if CLIENT_PREFIX_COMMAND in self.input_commands[j]:
+                env_commands.append(self.input_commands[j].split(CLIENT_DELIMITER))
+                del self.input_commands[j]
+            else:
+                j += 1
+        for env_cmd in env_commands:
+            if len(env_cmd) > 1 and env_cmd[1] == CLIENT_STOP:
+                self._enabled = False
+            elif len(env_cmd) > 2 and env_cmd[1] == CLIENT_LOOP:
+                self.loop_timeout = int(env_cmd[2])
+
     def _deliver_remote(self):
+        """Deliver results from reader to remote host."""
         sftp = self._ssh.open_sftp() 
         with sftp.open(self.output_path, 'wb') as f:
             f.write('\n'.join(self._reader.lines))
-        
+
+    def _remove_input_commands(self):
+        """Clear commands after successfull reading."""
+        sftp = self._ssh.open_sftp()
+        with sftp.open(self.input_path, 'wb') as f:
+            f.write('')
+
+
 class Reader(Thread):
     '''listen on serial port'''
     
@@ -117,8 +155,7 @@ class Reader(Thread):
                 if len(line) > 0:
                     self.lines.append(line)
 
-            
-            
+
 class Writer(Thread):
     '''write from stdin to serial port'''
     
@@ -131,7 +168,6 @@ class Writer(Thread):
         for command in self.commands:
             time.sleep(0.2) # sleep for 200ms before checking serial again
             with serial_lock:
-                print 'command:', str(command),'done'
                 self._serial.write(str(command)+'\n')
             
 serial_lock = RLock()
